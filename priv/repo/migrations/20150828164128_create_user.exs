074dfb615e9bfd02e373888d07f30bdcc726ae2b
Synthmax <max@synthmax.dk>
2015-08-28 19:41:30 +0200

    -

--- priv/repo/migrations/20150828164128_create_user.exs
+++ priv/repo/migrations/20150828164128_create_user.exs
@@ -2,14 +2,14 @@ defmodule Digiramp.Repo.Migrations.CreateUser do
   use Ecto.Migration
 
   def change do
-    create table(:users) do
-      add :name, :string
-      add :email, :string
-      add :bio, :string
-      add :number_of_pets, :integer
-
-      timestamps
-    end
+    #create table(:users) do
+    #  add :name, :string
+    #  add :email, :string
+    #  add :bio, :string
+    #  add :number_of_pets, :integer
+    #
+    #  timestamps
+    #end
 
   end
 end
