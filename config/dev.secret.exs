fec049d917d6813dbc879e51c19ab56b57207ef5
Synthmax <max@synthmax.dk>
2015-09-07 16:10:22 +0200

    -

---
+++ config/dev.secret.exs
@@ -0,0 +1,11 @@
+use Mix.Config
+
+# In this file, we keep test configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :digiramp, Digiramp.Endpoint,
+  secret_key_base: "XMzok4rjYgZO0zfzgN7DAet/pGEZNraRRmQKpKQkjbG56FM+fRcNZ+6IHDo64j8G",
+  database_postgresql_user_name: "synthmax",
+  database_postgresql_NAME: "digiramp_2_test",
+  access_key_id: "AKIAJOHXU7T6MPWRNU3A",
+  secret_access_key: "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"
--- config/test.exs
+++ config/test.exs
@@ -4,7 +4,7 @@ use Mix.Config
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :digiramp, Digiramp.Endpoint,
-  http: [port: 4001]
+  http: [port: 4001],
   server: false
 
 # Print only warnings and errors during test
